853c7f03eea392ea681029f26aaeba39e7ce5bdd
Aayushi Agrawal <aayushi.agrawal13@gmail.com>
2018-02-07 12:49:51 +0530

    code clean

--- src/components/ImdbRating.js
+++ src/components/ImdbRating.js
@@ -2,26 +2,13 @@ import React  from 'react';
 import Header from './Header';
 import WeatherImdb from './common';
 
-var data= "";
-export default class ImdbRating extends React.Component{
+export default class ImdbRatingPage extends React.Component{
 
-    // getImdbRatimg = async() =>{
-    //     var imdbURL = 'http://www.omdbapi.com/?i=tt3896198&apikey=e40c17f7';
-    //     const imdb_api_call = await fetch(imdbURL);
-    //     data = await imdb_api_call.json(); //converting api-call to json format for readability
-    //     console.log(data);
-    //     this.setState({});
-    //     console.log(data.Title);
-    //  } 
-    //  componentDidMount(){
-    //      this.getImdbRatimg();
-    // }
- 
     render(){
         return( 
             <div>
                 <Header/>
-                <WeatherImdb data={data} />
+                <WeatherImdb  />
             </div>
         )
     }
